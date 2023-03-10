acfacdc1297d410526aa91c41b437f49bfa3950c
IvanG <ivang@mail.com>
2023-03-10 15:28:59 +0100

    no need to be class, changed to namespace

--- src/utils/interpolation.h
+++ src/utils/interpolation.h
@@ -9,26 +9,26 @@
 #include <QAbstractTableModel>
 #include <cmath>
 
-class Interpolation
+namespace Interpolation
 {
-public:
-    static double cubicInterpolate(double p[4], double x)
+    constexpr double cubicInterpolate(double p[4], double x)
     {
         return p[1] + 0.5 * x * (p[2] - p[0] + x *(2.0 * p[0] - 5.0 * p[1]
                 + 4.0 * p[2] - p[3] + x * (3.0 * (p[1] - p[2]) + p[3] - p[0])));
     }
 
-    static double bicubicInterpolate(double p[4][4], double x, double y)
+    constexpr double bicubicInterpolate(double p[4][4], double x, double y)
     {
-        double arr[4];
-        arr[0] = cubicInterpolate(p[0], x);
-        arr[1] = cubicInterpolate(p[1], x);
-        arr[2] = cubicInterpolate(p[2], x);
-        arr[3] = cubicInterpolate(p[3], x);
+        double arr[4] = {
+                cubicInterpolate(p[0], x),
+                cubicInterpolate(p[1], x),
+                cubicInterpolate(p[2], x),
+                cubicInterpolate(p[3], x)
+        };
         return cubicInterpolate(arr, y);
     }
 
-    static double bicubicInterpolate(QRectF borderRect, QAbstractTableModel *basePoints, double x, double y)
+    constexpr double bicubicInterpolate(QRectF borderRect, QAbstractTableModel *basePoints, double x, double y)
     {
         // Setup grid
         int gridPointsX = basePoints->columnCount();
@@ -47,7 +47,7 @@ public:
         if (ix > basePoints->columnCount() - 2) ix = basePoints->columnCount() - 2;
         if (iy > basePoints->rowCount() - 2) iy = basePoints->rowCount() - 2;
 
-        double p[4][4];
+        double p[4][4]{};
 
         p[0][0] = basePoints->data(basePoints->index((iy > 0 ? iy - 1 : iy), (ix > 0 ? ix - 1 : ix)), Qt::UserRole).toDouble();
         p[0][1] = basePoints->data(basePoints->index((iy > 0 ? iy - 1 : iy), ix), Qt::UserRole).toDouble();
@@ -72,7 +72,7 @@ public:
         // Interpolate
         return Interpolation::bicubicInterpolate(p, x / gridStepX - ix, y / gridStepY - iy);
     }
-};
+}
 
 #endif // INTERPOLATION
 
