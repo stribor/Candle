30c700372f2e84017f1c46d2a8feae9130c80eb9
IvanG <ivang@mail.com>
2023-03-10 16:20:38 +0100

    LineSegment moved most implementations to .h

--- src/parser/linesegment.h
+++ src/parser/linesegment.h
@@ -19,94 +19,127 @@ public:
     using Container = std::vector<LineSegment>;
 #else
     using Container = QVector<LineSegment>;
-//    using Container = QList<LineSegment>;
 #endif
-    LineSegment();
-    LineSegment(QVector3D a, QVector3D b, int num, PointSegment const &ps, bool isMetric);
-    LineSegment(LineSegment const &initial);
-    ~LineSegment();
+    LineSegment() = default;
+    LineSegment(QVector3D a, QVector3D b, int num, PointSegment const &ps, bool isMetric) : LineSegment() {
+        m_first = a;
+        m_second = b;
+        m_lineNumber = num;
 
-    int getLineNumber() const;
-    QList<QVector3D> getPointArray();
-    QList<double> getPoints();
+        m_isArc = ps.isArc();
+        if (m_isArc)
+            m_isClockwise = ps.isClockwise();
+        m_plane = ps.plane();
+        m_isFastTraverse = ps.isFastTraverse();
+        m_isZMovement = ps.isZMovement();
+        m_isMetric = isMetric;
+        m_isAbsolute = ps.isAbsolute();
+        m_speed = ps.getSpeed();
+        m_spindleSpeed = ps.getSpindleSpeed();
+        m_dwell = ps.getDwell();
+    }
+    LineSegment(LineSegment const &initial){
+        m_toolhead = initial.getToolhead();
+        m_isZMovement = initial.isZMovement();
+        m_isArc = initial.isArc();
+        m_isFastTraverse = initial.isFastTraverse();
+        m_drawn = initial.drawn();
+        m_first = initial.getStart();
+        m_second = initial.getEnd();
+        m_lineNumber = initial.getLineNumber();
+        m_speed = initial.getSpeed();
+        m_isMetric = initial.isMetric();
+        m_isAbsolute = initial.isAbsolute();
+        m_isHightlight = initial.isHightlight();
+        m_vertexIndex = initial.vertexIndex();
+    }
 
-    QVector3D const &getStart() const;
-    void setStart(QVector3D vector);
+    ~LineSegment() = default;
 
-    QVector3D const &getEnd() const;
-    void setEnd(QVector3D vector);
+    [[nodiscard]] int getLineNumber() const { return m_lineNumber; }
+    [[nodiscard]] QList<QVector3D> getPointArray();
+    [[nodiscard]] QList<double> getPoints();
 
-    void setToolHead(int head);
-    int getToolhead() const;
-    void setSpeed(double s);
-    double getSpeed() const;
-    void setIsZMovement(bool isZ);
-    bool isZMovement() const;
-    void setIsArc(bool isA);
-    bool isArc() const;
-    void setIsFastTraverse(bool isF);
-    bool isFastTraverse() const;
+    [[nodiscard]] QVector3D const &getStart() const { return m_first; }
+    void setStart(QVector3D vector) { m_first = vector; }
+
+    QVector3D const &getEnd() const { return m_second; }
+    void setEnd(QVector3D vector) { m_second = vector; }
+
+    void setToolHead(int head) { m_toolhead = head; }
+    [[nodiscard]] int getToolhead() const { return m_toolhead; }
+    void setSpeed(double s) { m_speed = s; }
+    [[nodiscard]] double getSpeed() const { return m_speed; }
+    void setIsZMovement(bool isZ) { m_isZMovement = isZ; }
+    [[nodiscard]] bool isZMovement() const { return m_isZMovement; }
+    void setIsArc(bool isA) { m_isArc = isA; }
+    [[nodiscard]] bool isArc() const { return m_isArc; }
+    void setIsFastTraverse(bool isF) { m_isFastTraverse = isF; }
+    [[nodiscard]] bool isFastTraverse() const { return m_isFastTraverse; }
 
     bool contains(const QVector3D &point) const;
 
-    bool drawn() const;
-    void setDrawn(bool drawn);
+    [[nodiscard]] bool drawn() const { return m_drawn; }
+    void setDrawn(bool drawn) { m_drawn = drawn; }
 
-    bool isMetric() const;
-    void setIsMetric(bool isMetric);
+    [[nodiscard]] bool isMetric() const { return m_isMetric; }
+    void setIsMetric(bool isMetric) { m_isMetric = isMetric; }
 
-    bool isAbsolute() const;
-    void setIsAbsolute(bool isAbsolute);
+    [[nodiscard]] bool isAbsolute() const { return m_isAbsolute; }
+    void setIsAbsolute(bool isAbsolute) { m_isAbsolute = isAbsolute; }
 
-    bool isHightlight() const;
-    void setIsHightlight(bool isHightlight);
+    [[nodiscard]] bool isHightlight() const { return m_isHightlight; }
+    void setIsHightlight(bool isHightlight) { m_isHightlight = isHightlight; }
 
-    int vertexIndex() const;
-    void setVertexIndex(int vertexIndex);
+    [[nodiscard]] int vertexIndex() const { return m_vertexIndex; }
+    void setVertexIndex(int vertexIndex) { m_vertexIndex = vertexIndex; }
 
-    double getSpindleSpeed() const;
-    void setSpindleSpeed(double spindleSpeed);
+    [[nodiscard]] double getSpindleSpeed() const { return m_spindleSpeed; }
+    void setSpindleSpeed(double spindleSpeed) { m_spindleSpeed = spindleSpeed; }
 
-    double getDwell() const;
-    void setDwell(double dwell);
+    [[nodiscard]] double getDwell() const { return m_dwell; }
+    void setDwell(double dwell) { m_dwell = dwell; }
 
-    bool isClockwise() const;
-    void setIsClockwise(bool isClockwise);
+    [[nodiscard]] bool isClockwise() const { return m_isClockwise; }
+    void setIsClockwise(bool isClockwise) { m_isClockwise = isClockwise; }
 
-    PointSegment::planes plane() const;
-    void setPlane(const PointSegment::planes &plane);
+    [[nodiscard]] PointSegment::planes plane() const { return m_plane; }
+    void setPlane(const PointSegment::planes &plane) { m_plane = plane; }
 
 private:
-    double m_speed;
-    double m_spindleSpeed;
-    double m_dwell;
-    QVector3D m_first, m_second;
-    int m_toolhead;
+    double m_speed{};
+    double m_spindleSpeed{};
+    double m_dwell{};
+    QVector3D m_first{};
+    QVector3D m_second{};
+
+    //DEFAULT TOOLHEAD ASSUMED TO BE 0!
+    int m_toolhead{0};
 
     // Line properties
-    int m_lineNumber;
-    int m_vertexIndex;
-    PointSegment::planes m_plane;
+    int m_lineNumber{-1};
+    int m_vertexIndex{-1};
+    PointSegment::planes m_plane{};
 #if 0
     // use less memory but bit slower
-    bool m_isZMovement:1;
-    bool m_isArc:1;
-    bool m_isMetric:1;
-    bool m_isFastTraverse:1;
-    bool m_isAbsolute:1;
-    bool m_isClockwise:1;
-    bool m_isHightlight:1;
-    bool m_drawn:1;
+    bool m_isZMovement:1{false};
+    bool m_isArc:1{false};
+    bool m_isMetric:1{true};
+    bool m_isFastTraverse:1{false};
+    bool m_isAbsolute:1{true};
+    bool m_isClockwise:1{false};
+    bool m_isHightlight:1{false};
+    bool m_drawn:1{false};
 #else
     // bit faster but use bit more memory
-    bool m_isZMovement;
-    bool m_isArc;
-    bool m_isMetric;
-    bool m_isFastTraverse;
-    bool m_isAbsolute;
-    bool m_isClockwise;
-    bool m_isHightlight;
-    bool m_drawn;
+    bool m_isZMovement{false};
+    bool m_isArc{false};
+    bool m_isMetric{true};
+    bool m_isFastTraverse{false};
+    bool m_isAbsolute{true};
+    bool m_isClockwise{false};
+    bool m_isHightlight{false};
+    bool m_drawn{false};
 #endif
 };
 
